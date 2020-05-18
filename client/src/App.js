4d124572aeadc58d75796d9f3de013a18ebaacf5
KSClopton <kyle.clopton@outlook.com>
2020-05-18 18:04:14 -0400

    finished all tasks

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -13,6 +16,7 @@ const App = () => {
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovieList(response.data);
+          
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -26,10 +30,16 @@ const App = () => {
   };
 
   return (
+    <Router>
     <div>
+      
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route  exact path='/'  component={() => <MovieList movies={movieList}/>}/>
+
+      <Route path="/movies/:id" component={Movie} />
+      
     </div>
+  </Router>
   );
 };
 
